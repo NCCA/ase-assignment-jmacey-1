e6983295ad35761dad40768c001a01269c39270f
jmacey <jmacey@bournemouth.ac.uk>
2023-11-01 13:25:04 +0000

    more week2 work done

--- include/PointVizWriter.h
+++ include/PointVizWriter.h
@@ -2,6 +2,8 @@
 #define POINTVIZWRITER_H_
 #include <string_view>
 #include <fstream>
+#include <ostream>
+
 #include <variant>
 
 namespace pv
@@ -13,20 +15,36 @@ namespace pv
     public :
         // @brief construct with filename
         // @param _filename the name and path of file to open.
-        PointVizWriter(std::string_view _filename);
+        PointVizWriter(std::string_view _filename, FrameTimeType _frameType=FrameTimeType::INT);
         ~PointVizWriter();
         // @brief write data to the stream verbatim
         void write(std::string_view _str);
         // @brief write data to stream and newline.
         void writeln(std::string_view _str);
-        void setFrameType(FrameTimeType _t);
         FrameTimeType getFrameType() const;
+        bool setFrame(std::variant<int,float,double> _frame);
+        std::variant<int,float,double> frameNumber() const ;
 
+        /* code generate from co-pilot
+         * prompt :
+         * given this class how do I overload the << operator so I can write data to m_stream
+         * in the form of obj<<1..0<<' '<<2.0<<'\n';
+         * Code is copied verbatim
+         */
 
+        template<typename T>
+        friend PointVizWriter& operator<<(PointVizWriter& pvw, const T& data)
+        {
+            pvw.m_stream << data;
+            return pvw;
+        }
+        // end citation.
     private :
         std::ofstream m_stream;
         FrameTimeType m_frameType=FrameTimeType::INT;
-        std::variant<int,float,double> m_frameNumber=0;
+        std::variant<int,float,double> m_frameNumber;
     };
 }
+
+
 #endif
--- tests/PointVizWriterTests.cpp
+++ tests/PointVizWriterTests.cpp
@@ -20,23 +20,56 @@ TEST(PointVizWriter,writeTest)
     o.writeln("0.1,0.2,0.3");
 }
 
-TEST(PointVizWriter,frameType)
-{
-    pv::PointVizWriter o("t.csv");
-    EXPECT_TRUE(o.getFrameType() == pv::FrameTimeType::INT);
-    o.setFrameType(pv::FrameTimeType::DOUBLE);
-    EXPECT_TRUE(o.getFrameType() == pv::FrameTimeType::DOUBLE);
-    o.setFrameType(pv::FrameTimeType::FLOAT);
-    EXPECT_TRUE(o.getFrameType() == pv::FrameTimeType::FLOAT);
-    fs::remove("t.csv");
-}
 
 TEST(PointVizWriter,testFrame)
 {
     pv::PointVizWriter o("t.csv");
-    o.setFrame(1);
+    EXPECT_TRUE(o.setFrame(1));
+    EXPECT_FALSE(o.setFrame(0));
+}
+
+TEST(PointVizWriter,writeFrameTestInt)
+{
+    pv::PointVizWriter o("test.csv");
+    EXPECT_TRUE(fs::exists("test.csv"));
+    for(int i=0; i<10; ++i)
+    {
+        o.setFrame(i);
+        o.write("0.1,0.2,0.3\n");
+        o.writeln("0.1,0.2,0.3");
+    }
 }
 
 
+TEST(PointVizWriter,writeFrameTestFloat)
+{
+    pv::PointVizWriter o("test.csv",pv::FrameTimeType::FLOAT);
+    EXPECT_TRUE(fs::exists("test.csv"));
+    for(float i=0.0f; i<1.0f; i+=0.1f)
+    {
+        o.setFrame(i);
+        o.write("0.1,0.2,0.3\n");
+        o.writeln("0.1,0.2,0.3");
+    }
+}
 
+TEST(PointVizWriter,writeFrameTestDouble)
+{
+    pv::PointVizWriter o("testDouble.csv",pv::FrameTimeType::DOUBLE);
+    EXPECT_TRUE(fs::exists("testDouble.csv"));
+    for(double i=0.00000; i<0.01; i+=0.001)
+    {
+        o.setFrame(i);
+        o.write("0.1,0.2,0.3\n");
+        o.writeln("0.1,0.2,0.3");
+    }
+}
+TEST(PointVizWriter,overLoad) {
+    pv::PointVizWriter o("testDouble.csv", pv::FrameTimeType::DOUBLE);
+    EXPECT_TRUE(fs::exists("testDouble.csv"));
+    for (double i = 0.0; i < 0.05; i += 0.01) {
+        o.setFrame(i);
+        o << i << ' ' << i << ' ' <<  i << " # this is comment \n";
 
+    }
+}
